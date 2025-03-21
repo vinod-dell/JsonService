5412c0cc86c3edcbcad42dbfb75c6b2e7e41c001
vinod-dell <68218503+vinod-dell@users.noreply.github.com>
2025-03-21 14:22:10 +0530

    Update HelloWorld.java

--- src/main/java/com/hkdemircan/hello/HelloWorld.java
+++ src/main/java/com/hkdemircan/hello/HelloWorld.java
@@ -1,18 +1,23 @@
-package com.hkdemircan.hello;
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HelloWorld {
 
     @GetMapping("/")
-    public String getMessage(){
-        try{ 
+    public Map<String, String> getMessage() {
+        try {
             Thread.sleep(80000);
-        } catch (Exception e){
-           
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
-        return "sleep after 80sec";
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Sleep after 80 sec");
+
+        return response;
     }
 }
